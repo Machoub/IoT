02261e1dd8e3bf82424cba4ad2e22dd9a75965dc
Debian <debian@debian12>
2025-10-10 18:37:32 +0200

    P3

--- p3/conf/argocd.sh
+++ p3/conf/argocd.sh
@@ -1,9 +1,9 @@
 # Delete namespaces then recreate it
-sudo kubectl delete namespace argocd
-sudo kubectl delete namespace dev
+kubectl delete namespace argocd
+kubectl delete namespace dev
 
 #install argocd
-sudo kubectl create namespace argocd
-sudo kubectl create namespace dev
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create namespace argocd
+kubectl create namespace dev
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
--- p3/conf/k3d.sh
+++ p3/conf/k3d.sh
@@ -5,11 +5,17 @@ echo "[+] Installing docker"
 sudo apt-get install -qqy docker.io
 sudo usermod -aG docker $USER
 
-echo "[+] Installing k3d"
-sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
 echo "[+] Installing kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl
 
+echo "[+] Installing k3d"
+sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+echo "[+] Creating Kubernetes cluster with k3d"
+k3d cluster delete machoubaS
+k3d cluster create machoubaS
+
+echo "[+] Listing k3d clusters"
+k3d cluster list
